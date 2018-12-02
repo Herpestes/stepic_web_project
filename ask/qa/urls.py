4aa4df81e80794c8b731c365ab841a521b0a3d75
Sergey <skmorozov@gmail.com>
2018-12-02 19:14:28 +0400

    dfg

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from . import views
 
 
-urlpatterns = patterns('qa.views',
-    url(r'^$', 'index', name='index'),
-    url(r'^login/', 'login', name='login'),
-    url(r'^signup/', 'signup', name='signup'),
-    url(r'^question/(?P<quest_id>[0-9]+)/$', 'question', name='question'),
-    url(r'^ask/', 'ask', name='ask'),
-    url(r'^answer/', 'answer', name='answer'),
-    url(r'^popular/', 'popular', name='popular'),
-    url(r'^new', 'test', name='new'),
-)
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^signup/', views.signup, name='signup'),
+    url(r'^question/(?P<quest_id>[0-9]+)/$', views.question, name='question'),
+    url(r'^ask/', views.ask, name='ask'),
+    url(r'^answer/', views.answer, name='answer'),
+    url(r'^popular/', views.popular, name='popular'),
+    url(r'^new', views.test, name='new'),
+]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,6 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+
 def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def popular(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def answer(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def ask(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def question(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def signup(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def login(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def index(request, *args, **kwargs):
     return HttpResponse('OK')
 

4a3a1b51790aa990cf59cbea87a2c8d64932d543
Sergey <skmorozov@gmail.com>
2018-12-02 17:40:37 +0400

    dff

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
+
+git config --global user.name "Sergey"
+git config --global user.email skmorozov@gmail.com

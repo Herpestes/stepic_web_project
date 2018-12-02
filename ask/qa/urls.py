53bef2b61ee0a43fc784a03e6f661173c45fac02
Sergey <skmorozov@gmail.com>
2018-12-02 17:34:09 +0400

    fgh

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('qa.views',
     url(r'^ask/', 'ask', name='ask'),
     url(r'^answer/', 'answer', name='answer'),
     url(r'^popular/', 'popular', name='popular'),
-    url(r'^new', 'new', name='new'),
+    url(r'^new', 'test', name='new'),
 )
